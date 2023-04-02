51576e3ed04d5a4e966f69c7ab3e639a041e1eea
Vaz <felipe.vaz@localiza.com>
2023-04-02 20:52:04 -0300

    testing FP

--- test.py
+++ test.py
@@ -8,26 +8,27 @@ initial_date = datetime(year=2023, month=3, day=28, hour=23, minute=59, second=5
 today_date = datetime.now()
 delta_date = today_date - initial_date
 
-def fetch_next_day(days):
-    next_day = initial_date + pd.Timedelta(days=days)
+def fetch_next_day():
+    next_day = initial_date + pd.Timedelta(days=1)
     return next_day
 
-def extract_list_of_dates(next_day):
-    list = fetch_next_day(next_day)
-    list_of_dates = [list[i] for i in range(delta_date.days+1)]
+def extract_list_of_dates():
+    next_day = fetch_next_day()
+    list_of_dates = [next_day[i] for i in range(delta_date.days+1)]
     return list_of_dates
             
-def extract_data(list_of_dates):
+def extract_data():
+    list_of_dates = extract_list_of_dates()
     client = Socrata(domain="data.sfgov.org",
                  app_token="",
                  username="",
                  password="")
-    query = f'select count(*) where incident_datetime < "{[list_of_dates[i] for i in range(list_of_dates)]}]"'
+    query = f'select count(*) where incident_datetime < "{[list_of_dates[i] for i in range(list_of_dates[i])]}]"'
     # requests = client.get("wg3w-h783", query=[])
     # results = client.get("wg3w-h783", query=query)
     return query
 
-print(extract_data(list_of_dates=extract_list_of_dates(fetch_next_day())))
+print(extract_data())
 # print(fetch_next_day(1), delta_date.days+1, next_day, type(next_day))
 
 # print(initial_date.isoformat(), next_day.isoformat())
