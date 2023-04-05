6beaa626caccdb58f0848dfbd968313c0e7f6e06
Vaz <felipe.vaz@localiza.com>
2023-04-05 09:07:27 -0300

    returning dataframe from socrata

--- test.py
+++ test.py
@@ -4,16 +4,18 @@ import pandas as pd
 import pandas as pd
 from sodapy import Socrata
 import json
+from time import perf_counter
 
 with open('config.json', 'r') as f:
     config = json.load(f)
 
 def daterange(start_date, end_date):
-    for n in range(int((end_date - start_date).days)):
+    for n in range(int((end_date - start_date).days)+1):
         yield start_date + timedelta(n)
 
-initial_date = date(2023,4,1)
-today_date = date.today()
+initial_date = date(2022,1,1)
+today_date = date(2022,1,2)
+# date.today()
 
 def extract_list_of_dates():
     # next_day = fetch_next_day()
@@ -21,13 +23,23 @@ def extract_list_of_dates():
         yield (datetime(year=single_date.year,month=single_date.month,day=single_date.day) + timedelta(hours=23,minutes=59,seconds=59,microseconds=999999)).isoformat()
             
 def extract_data():
+    output = []
+    elapsed = []
     for dates in extract_list_of_dates():
+        start_time = perf_counter()
         client = Socrata(domain="data.sfgov.org",
-                    app_token=config.get('app_token'),
-                    username=config.get('username'),
-                    password=config.get('password'))
-        results = client.get("wg3w-h783", query=f'select count(*) where incident_datetime <= "{dates}"')
+                    app_token=config.get('credentials').get('app_token'),
+                    username=config.get('credentials').get('username'),
+                    password=config.get('credentials').get('password'))
+        yesterday = datetime.strptime(dates,"%Y-%m-%dT%H:%M:%S.%f") - timedelta(1)
+        results = client.get("wg3w-h783", query=f'select date_trunc_ymd(incident_datetime) AS incident_datetime, count(incident_datetime) AS countReg where incident_datetime between "{yesterday.isoformat()}" and "{dates}" group by date_trunc_ymd(incident_datetime) order by incident_datetime')
         results_df = pd.DataFrame.from_records(results)
+        stop_time = perf_counter()
+        elapsed_time = str(stop_time - start_time)
+        elapsed_time_df = pd.DataFrame.from_records(elapsed_time, columns=['elapsed_time'])
+        output.append(results_df) 
+        results_df['elapsed_time'] =  elapsed_time_df
+        results_df = pd.concat(output)
     return results_df
 
 # for value in extract_list_of_dates():
--- test_2.py
+++ test_2.py
@@ -1,4 +1,5 @@
 from datetime import date, datetime, timedelta
+import json
 
 def daterange(start_date, end_date):
     for n in range(int((end_date - start_date).days)):
@@ -11,6 +12,17 @@ def myfunc():
     for single_date in daterange(start_date, end_date):
         mydate = datetime(year=single_date.year,month=single_date.month,day=single_date.day) + timedelta(hours=23,minutes=59,seconds=59,microseconds=999999)
         yield mydate.isoformat()
-    
-for value in myfunc():
-    print(value)
+
+def mynewfunc():
+    for dates in myfunc():
+        print(datetime.strptime(dates,"%Y-%m-%dT%H:%M:%S.%f") - timedelta(1), dates)
+
+mynewfunc()
+
+# for value in myfunc():
+#     print(value)
+
+# with open('config.json', 'r') as f:
+#     config = json.load(f)
+
+# print(config["credentials"]["app_token"])
