532ab1616b9e21b4f61598b496d8d36be9d0462c
Vaz <felipe.vaz@localiza.com>
2023-04-17 07:02:04 -0300

    adding bucket connection

--- request_socrata_soda.py
+++ request_socrata_soda.py
@@ -12,6 +12,7 @@ start_date = date(2018, 1, 1)
 end_date = date.today()
 day_range = int((end_date - start_date).days)+1
 number_of_days = 3
+bucketname = "socrata-vaz-data"
 
 def extract_list_of_dates(start_date, number_of_days):
     list_of_dates=[]
@@ -21,7 +22,9 @@ def extract_list_of_dates(start_date, number_of_days):
                 list_of_dates.append((datetime(year=start_date.year,month=start_date.month,day=start_date.day) + timedelta(day, hours=23,minutes=59,seconds=59,microseconds=999999)).isoformat())
     return list_of_dates
 
-def extract_load_data():
+def extract_load_data(bucketname, start_date, number_of_days):
+    storage_client = storage.Client.from_service_account_json('C:\\Users\\144553\\OneDrive - Localiza\\Vaz\\PDI\\GCP\\credentials\\vaz-data-1f9278fd0828.json')
+    bucket = storage_client.bucket(bucket_name=bucketname)
     for date in extract_list_of_dates(start_date=start_date, number_of_days=number_of_days):
         params = {'$limit': 10,
                 '$where': 'incident_datetime <=' + "'" + str(date) + "'"
@@ -32,27 +35,53 @@ def extract_load_data():
                         )
         if r.status_code != 200:
             raise RuntimeError('Can''t retrieve latest timestamp.' + r.text)
-        
-        filename = os.path.abspath(os.curdir) + "\\files\\" + str(config.get('dataset').get('san_francisco_data')) + "-" + str(time.time_ns()) + ".json"
-        os.makedirs(os.path.dirname(filename), exist_ok=True)
-        with open(filename, "w") as write_file:
+        # filename = os.path.abspath(os.curdir) + "\\files\\" + str(config.get('dataset').get('san_francisco_data')) + "-" + str(time.time_ns()) + ".json"
+        filename = str(config.get('dataset').get('san_francisco_data')) + "-" + str(time.time_ns()) + ".json"
+        # os.makedirs(os.path.dirname(filename), exist_ok=True)
+        blob = bucket.blob(filename)
+        with blob.open("w") as write_file:
             json.dump(r.json(), write_file, indent=4)
 
     json_str = json.dumps(r.json(), indent=4)
     # json_obj = r.json()
     return json_str
 
-def list_blobs(bucket_name):
-    """Lists all the blobs in the bucket."""
-    # bucket_name = "socrata-vaz-data"
+extract_load_data(bucketname=bucketname, start_date=start_date, number_of_days=4)
 
-    storage_client = storage.Client()
+def list_buckets_blobs(bucket_name):
+    """Lists all buckets."""
 
-    # Note: Client.list_blobs requires at least package version 1.17.0.
+    storage_client = storage.Client.from_service_account_json('C:\\Users\\144553\\OneDrive - Localiza\\Vaz\\PDI\\GCP\\credentials\\vaz-data-1f9278fd0828.json')
+    buckets = storage_client.list_buckets()
     blobs = storage_client.list_blobs(bucket_name)
 
-    # Note: The call returns a response only when the iterator is consumed.
+    for bucket in buckets:
+        print(bucket.name)
     for blob in blobs:
         print(blob.name)
 
-list_blobs("socrata-vaz-data")
+list_buckets_blobs("socrata-vaz-data")
+
+def bucket(bucket_name):
+    """Write and read a blob from GCS using file-like IO"""
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+
+    # The ID of your new GCS object
+    # blob_name = "storage-object-name"
+
+    storage_client = storage.Client.from_service_account_json('C:\\Users\\144553\\OneDrive - Localiza\\Vaz\\PDI\\GCP\\credentials\\vaz-data-1f9278fd0828.json')
+    bucket = storage_client.bucket(bucket_name)
+    # blob = bucket.blob(blob_name)
+
+    return bucket
+
+    # Mode can be specified as wb/rb for bytes mode.
+    # See: https://docs.python.org/3/library/io.html
+    with blob.open("w") as f:
+        f.write("Hello world")
+
+    with blob.open("r") as f:
+        print(f.read())
+
+# write_read("socrata-vaz-data", "first_commit")
