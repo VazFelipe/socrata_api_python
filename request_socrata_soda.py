1eb552ed57be1878bc186979dfbcbb626194fe43
Vaz <felipe.vaz@localiza.com>
2023-04-20 07:06:17 -0300

    updating fetch_max_date_from_bucket()

--- request_socrata_soda.py
+++ request_socrata_soda.py
@@ -5,10 +5,14 @@ from datetime import date, datetime, timedelta, time
 import time
 from google.cloud import storage
 
+# storage.Client.from_service_account_json(config.get('gcp').get('credentials').get('folder'))
+# filename = os.path.abspath(os.curdir) + "\\files\\" + str(config.get('dataset').get('san_francisco_data')) + "-" + str(time.time_ns()) + ".json"
+# os.makedirs(os.path.dirname(filename), exist_ok=True)
+
 with open('config.json', 'r') as f:
     config = json.load(f)
 
-start_date = date(2018, 1, 1) 
+start_date = date(2018, 1, 5) 
 end_date = date.today()
 day_range = int((end_date - start_date).days)+1
 number_of_days = 3
@@ -26,32 +30,29 @@ def bucket_blob(bucket_name, blob_name):
     The ID of your new GCS object
     blob_name = "storage-object-name"
     """
-    client = storage_client() #storage.Client.from_service_account_json(config.get('gcp').get('credentials').get('folder'))
+    client = storage_client()
     bucket = client.bucket(bucket_name)
     blob = bucket.blob(blob_name)
 
     return bucket, blob
 
-def fetch_max_date_from_bucket(bucket_name):
-    client = storage_client()
-    return client
-
-def extract_list_of_dates(start_date, number_of_days):
+def extract_list_of_dates(start_date, number_of_days, bucket_name):
+    """ Define """
     list_of_dates=[]
     for day in range(day_range):
         if day <= day_range:
             if day <= number_of_days-1:
                 list_of_dates.append((datetime(year=start_date.year,month=start_date.month,day=start_date.day) + timedelta(day, hours=23,minutes=59,seconds=59,microseconds=999999)).isoformat())
+                if fetch_max_date_from_bucket(bucket_name):
+                    list_of_dates = [date for date in list_of_dates if date > fetch_max_date_from_bucket()]
     return list_of_dates
 
 def extract_load_data(bucket_name, start_date, number_of_days):
-    # filename = os.path.abspath(os.curdir) + "\\files\\" + str(config.get('dataset').get('san_francisco_data')) + "-" + str(time.time_ns()) + ".json"
-    # os.makedirs(os.path.dirname(filename), exist_ok=True)
-
+    """ Define """
     if number_of_days == 0:
         message = "Require the number_of_days greater than or equal to 1 to extract data"
     else: 
-        for date in extract_list_of_dates(start_date=start_date, number_of_days=number_of_days):
+        for date in extract_list_of_dates(start_date, number_of_days, bucket_name):
             
             params = {'$limit': 10,
                     '$where': 'incident_datetime <=' + "'" + str(date) + "'"
@@ -66,7 +67,7 @@ def extract_load_data(bucket_name, start_date, number_of_days):
             else:
                 json.dumps(r.json(), indent=4)
             
-            blob_name = "socrata/" + "dataset_" + str(config.get('api').get('dataset').get('san_francisco_data')) + "_crimes_" + str(datetime.fromisoformat(date).strftime("%Y-%m-%d")) + "_loaded_" + str(time.time_ns()) + ".json"
+            blob_name = "socrata/" + str(datetime.fromisoformat(date).strftime("%Y-%m-%d")) + "_dataset_" + str(config.get('api').get('dataset').get('san_francisco_data')) + "_crimes_in_loaded_" + str(time.time_ns()) + ".json"
             blob = bucket_blob(bucket_name=bucket_name, blob_name=blob_name)[1]
             
             with blob.open("w") as write_file:
@@ -75,24 +76,25 @@ def extract_load_data(bucket_name, start_date, number_of_days):
     
     return message
 
-def list_buckets_blobs(bucket_name):
-    """Lists all buckets."""
-    client = storage_client() #storage.Client.from_service_account_json(config.get('gcp').get('credentials').get('folder'))
+def fetch_max_date_from_bucket(bucket_name):
+    """Return the max incident_date from blob.name in the bucket, otherwise an empty dict"""
+    client = storage_client()
     buckets = client.list_buckets()
     blobs = client.list_blobs(bucket_name)
 
-    for bucket in buckets:
-        bucket = bucket.name
-    storage_dict = dict(bucket = bucket)
-
-    blob_str = []
+    blob_list = []
     for blob in blobs:
-        blob_str.append(blob.name)
+        blob_name = blob.name
+        blob_adjusted = blob_name[8:18]
+        blob_list.append(blob_adjusted)
+    
+    blob_dict = dict(enumerate(blob_list))
 
-    # for a, b in enumerate(blob):
-    #     storage_dict[a] = b
+    for max_value in sorted(blob_dict, key=blob_dict.get, reverse=True):
+        return blob_dict[max_value]
 
-    return blob_str #storage_dict # bucket, blob
+    return blob_dict
 
 # print(extract_load_data(bucket_name=bucket_name, start_date=start_date, number_of_days=3))
-print(list_buckets_blobs(bucket_name))
+print(fetch_max_date_from_bucket(bucket_name))
+# print(extract_list_of_dates(start_date, 10, bucket_name))
