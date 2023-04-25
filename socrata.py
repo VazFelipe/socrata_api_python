85878aff84fc13c56f8335efd09f0e5899a481a1
Vaz <felipe.vaz@localiza.com>
2023-04-25 08:13:34 -0300

    adding Socrata class for api connection

--- socrata.py
+++ socrata.py
@@ -5,28 +5,19 @@ from dataclasses import dataclass
 with open('config.json', 'r') as f:
     config = json.load(f)
 
-get_url = config.get('api').get('domain').get('url')
+get_url = config.get('api').get('domain').get('url') + config.get('api').get('dataset').get('san_francisco_data') + '.json'
 get_headers = config.get('api').get('headers')
-params_where_clause = config.get('api').get('params')
+params = config.get('api').get('params')
 
 @dataclass
 class Socrata:
     get_url: str
-    get_headers =  dict
-    params_where_clause = str
-    params_limit_clause = int
+    get_headers: dict
+    params: dict
 
-    def api_connection(self, get_url, get_headers, params_where_clause, params_limit_clause):
-        self.get_url = get_url
-        self.get_headers = get_headers
-        self.params_where_clause = params_where_clause
-        self.params_limit_clause = params_limit_clause
-
-        response = requests.get(self.get_url, self.get_headers, self.params_where_clause, self.params_limit_clause)
-
+    def api_connection(self):
+        response = requests.get(self.get_url, headers=self.get_headers, params=self.params)
         return response
 
-print(__name__)
 if __name__ == '__main__':
-    # print(type(config.get('api').get('headers')), config.get('api').get('headers'))
-    print(params_where_clause)
+    Socrata().api_connection()
