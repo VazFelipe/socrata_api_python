e7194d59dc19dab37f35a1e8379c0e7d93e17c91
Vaz <felipe.vaz@localiza.com>
2023-05-02 07:52:53 -0300

    defining function to add timedelta in command line params

--- main.py
+++ main.py
@@ -17,11 +17,25 @@ parser = argparse.ArgumentParser(description="Extract the dataset named Police D
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 type_modes = ["LAST_DATE", "REFRESH"]
 parser.add_argument("-m", "--mode", type=str, choices=type_modes, default=type_modes[0], required=True, help="the reference is the last date")
-parser.add_argument("-s", "--start_date", type=datetime.fromisoformat, default="2018-01-01", required=("--mode="+type_modes[1] in argv), help="used with refresh: from the desired date since 2018-01-01")
+parser.add_argument("-s", "--start_date", type=datetime.fromisoformat, default="2018-01-01", required=("--mode="+type_modes[1] in argv), help="used with refresh mode: from the desired date since 2018-01-01")
+parser.add_argument("-e", "--end_date", type=datetime.fromisoformat, default=date.today(), required=("--mode="+type_modes[1] in argv), help="used with refresh mode: from the desired date since 2018-01-01")
+parser.add_argument("-t", "--test_test", type=datetime.fromisoformat, default=date.today(), required=("--mode="+type_modes[0] in argv), help="used with refresh mode: from the desired date since 2018-01-01")
 args = vars(parser.parse_args())
 
+
+
+args_up = Socrata.add_time_date_params(args)
+
+
 start_date = args["start_date"]
-start_date_iso = (datetime(start_date.year, start_date.month, start_date.day) + timedelta(0, hours=23,minutes=59,seconds=59,microseconds=999999)).isoformat()
-print("--mode="+type_modes[1] in argv, argv, start_date, start_date_iso)
+# start_date_iso = (datetime(start_date.year, start_date.month, start_date.day) + timedelta(0, 23, 59, 59, 999999)).isoformat()
+# print("--mode="+type_modes[1] in argv, argv, start_date, end_date, )
+print(args_up, type(args_up))
+
+# for key, value in args.items():
+#     if key.endswith("date"):
+#         args_dict_updated = {key: (value + timedelta(days=0, hours=23, minutes=59, seconds=59, microseconds=999999)).isoformat()}
+        # args.update(args_dict_updated)
+        # print(args_dict_updated)
 
 Socrata(get_url,get_headers,params)
--- socrata.py
+++ socrata.py
@@ -1,5 +1,6 @@
 import json
 import requests
+from datetime import timedelta
 from dataclasses import dataclass
 
 @dataclass
@@ -7,10 +8,18 @@ class Socrata:
     get_url: str
     get_headers: dict
     params: dict
+    args_dict: dict
 
     def api_connection(self):
         response = requests.get(self.get_url, headers=self.get_headers, params=self.params)
         return response
+    
+    def add_time_date_params(self):
+        for key, value in self.args_dict.items():
+            if key.endswith("date"):
+                args_dict_updated = {key: (value + timedelta(days=0, hours=23, minutes=59, seconds=59, microseconds=999999)).isoformat()}
+                self.args_dict.update(args_dict_updated)
+        return self.args_dict
 
 if __name__ == '__main__':
-    Socrata()
+    Socrata
